62d5f379a61c41c33729722c452c5703c47a875b
Jose Reimondez <josereimondez29@gmail.com>
2024-07-28 11:48:33 +0200

    seguimos intentandolo

--- next.config.js
+++ next.config.js
@@ -1,26 +1,10 @@
 // next.config.js
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true, // Opcional, pero útil para identificar problemas en React
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: 'http://127.0.0.1:8000/api/:path*', // Proxy a tu backend Django
-      },
-    ];
-  },
-  webpack(config, { isServer }) {
-    // Ajustes específicos para Webpack en el entorno del cliente
-    if (!isServer) {
-      config.resolve.fallback = { fs: false };
-    }
-    return config;
-  },
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
+
 
 
   
